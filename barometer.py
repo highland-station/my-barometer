16f0d22c6bfdd17a0bc368153e7443b38cc7381a
highland-station <tashiro.a.work@gmail.com>
2026-09-05 20:52:23 +0900

    Add files via upload

--- barometer.py
+++ barometer.py
@@ -2176,7 +2176,7 @@ body {
 
 .hour-row {
     display: grid;
-    grid-template-columns: 65px 135px 180px 65px 75px 80px 80px;
+    grid-template-columns: 65px 135px 180px 65px 75px 80px 80px !important;
     align-items: center;
     min-height: 58px;
     border-bottom: 1px solid #302a2c;
